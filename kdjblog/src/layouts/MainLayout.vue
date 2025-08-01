3e7af085a8a9be1b52f33e24fd1c8f998d4595aa
Shangith25 <skasunnethusuwi@gmail.com>
2025-08-01 14:37:22 +0400

    project 3 nw 01

---
+++ kdjblog/src/layouts/MainLayout.vue
@@ -0,0 +1,23 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+
+
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script setup>
+
+
+
+
+
+
+
+function toggleLeftDrawer () {
+  leftDrawerOpen.value = !leftDrawerOpen.value
+}
+</script>
